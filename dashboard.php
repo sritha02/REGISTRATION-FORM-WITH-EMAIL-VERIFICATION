b8de9174047480613e54353c82da353dc36f6b51
sritha02 <87262592+sritha02@users.noreply.github.com>
2024-01-24 11:21:49 +0530

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,24 @@
+<?php 
+$page_title="Dashboard";
+include("includes/header.php"); 
+include("includes/navbar.php");
+?>
+
+<div class="py5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>User dashboard</h4>
+                    </div>
+                    <div class="card-body">
+                    <h2>Access when you are logged in</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  
+</div>
+
+<?php include("includes/footer.php"); ?>
