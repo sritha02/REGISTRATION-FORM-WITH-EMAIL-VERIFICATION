c184cd6fd4673c4632759ee8f28bedefabeb95e3
sritha02 <87262592+sritha02@users.noreply.github.com>
2024-01-24 11:24:21 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php if(isset($page_title)) echo "$page_title"  ;?>=Logo name
+    </title>
+    <link href="href=https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" ></link>
+</head>
+<body>
