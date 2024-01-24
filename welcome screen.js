f960d309322f7c0bde76a5802c3157ffd40491be
sritha02 <87262592+sritha02@users.noreply.github.com>
2024-01-24 11:12:00 +0530

    Create welcome screen.js

---
+++ welcome screen.js
@@ -0,0 +1,12 @@
+// WelcomeScreen.js
+import React from 'react';
+
+const WelcomeScreen = ({ userName }) => {
+  return (
+    <div>
+      <h2>Welcome {userName}</h2>
+    </div>
+  );
+};
+
+export default WelcomeScreen;
