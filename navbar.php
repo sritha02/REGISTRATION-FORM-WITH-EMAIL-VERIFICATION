7150da92ad47dffb93ac6518e7e411aae7630342
sritha02 <87262592+sritha02@users.noreply.github.com>
2024-01-24 11:17:44 +0530

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<?php 
+$page_title="Home page";
+include("includes/header.php"); 
+include("includes/navbar.php");
+?>
+
+<div class="py5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+<h2> Login and registration system in PHP</h2>
+<h4>With email Verification</h4>
+            </div>
+        </div>
+
+    </div>
+      
+</div>
+
+<?php include("includes/footer.php"); ?>
